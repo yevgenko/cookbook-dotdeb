d7861dd7b5ea3aa6c0f4f976e61cb0bb9c564301
Yevgeniy A. Viktorov <wik@osmonitoring.com>
2010-09-01 08:02:22 +0300

    bump version number

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Yevgeniy Viktorov"
 maintainer_email "wik@rentasite.com.ua"
 license          "Apache 2.0"
 description      "Dotdeb repositories"
-version          "0.1.0"
+version          "0.1.1"
 recipe           "dotdeb", "main Dotdeb repository"
 recipe           "dotdeb::php53", "php53 Dotdeb repository"
 
